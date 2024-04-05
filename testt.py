e81d8449cfacefc1f144935c08f6deab4a9362bd
edw7777 <wanged1110@gmail.com>
2024-04-05 16:50:05 -0400

    working for loop

--- testt.py
+++ testt.py
@@ -1,7 +1,11 @@
 import requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=FF4KQM71L6X80J6O'
-r = requests.get(url)
-data = r.json()
+list = ["MSFT", "AAPL", "NVDA", "AMZN", "META", "GOOG", "BRK.B", 
+        "LLY", "AVGO", "TSLA", "JPM", "UNH", "V", "XOM", "JNJ", "MA", 
+        "PG", "HD", "MRK", "COST", "ABBV", "CRM", "CVX", "AMD", "NFLX"]
 
-print(data)
+for i in range(len(list)):
+  url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={list[0]}&interval=5min&apikey=FF4KQM71L6X80J6O"
+  r = requests.get(url)
+  data = r.json()
+  print(data)
