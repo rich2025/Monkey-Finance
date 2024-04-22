f2bd468764ddc082b8c2222c429e1d2f00daad9c
rich2025 <richy@bu.edu>
2024-04-21 23:23:10 -0400

    home page update

--- frontend/src/pages/1.Home/Home.jsx
+++ frontend/src/pages/1.Home/Home.jsx
@@ -1,9 +1,13 @@
 const Home = () => {
   return (
     <div>
-        <h1 style={{fontSize: '200px', textAlign: 'center', fontFamily: 'Comic Sans'}}>
-          Monkey Finance
+      <div>
+        <h1 style={{fontSize: '90px', textAlign: 'left', marginLeft: '50px', fontFamily: 'Helvetica', color: 'yellow', filter: 'brightness(90%)'}}> <b>
+        <span style = {{color: '#DFFF00', filter: 'brightness(95%)'}}>M</span>onke<span style = {{color: 'black'}}>y</span> <span style = {{color: '#DFFF00', filter: 'brightness(100%)'}}>F</span>inanc<span style = {{color: 'black'}}>e</span>
+          
+          </b>
           </h1>
+          </div>
         <p>Welcome to Monkey Finance!</p>
     </div>
   )
