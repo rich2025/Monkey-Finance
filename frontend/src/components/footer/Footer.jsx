072ad4119bdac955b6abb6d3e57de9f476dd8537
rich2025 <richy@bu.edu>
2024-04-23 12:56:01 -0400

    footer update

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,11 +2,25 @@
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full bg-gradient-to-b from-yellow-50 to-yellow-200 text-white px-10 py-7 ">
-      <h1 style = {{color: 'black'}}>
-        hi
-      </h1>
+    
+<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="me-4 font-semibold text-gray-500 sm:text-center dark:text-gray-400">
+            <img src="https://static.vecteezy.com/system/resources/previews/035/982/836/original/ai-generated-monkey-isolated-on-transparent-background-free-png.png" alt="Monkey Finance Logo" class="h-9 w-9 inline mr-2"></img>
+            Monkey Finance
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="/about" class="hover:underline me-4 md:me-6">About</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">Contributors</a>
+            </li>
+        </ul>
     </div>
+</footer>
+
+
   )
 }
 
