103b4277edfe3665a993de27e09dc2ad073e73f3
rich2025 <richy@bu.edu>
2024-04-23 18:07:06 -0400

    working on oauth

--- frontend/src/pages/6.Login/Login.jsx
+++ frontend/src/pages/6.Login/Login.jsx
@@ -1,12 +1,33 @@
+import { GoogleLogin } from 'react-google-login';
+
+const clientId = "556163547173-t4d85bper0npj74uuugb5rqh1jjbbq72.apps.googleusercontent.com";
+
+const onSuccess = (res) => {
+}
+
 const Login = () => {
+  function login() {
     return (
-      <div>
-        <h1>About</h1>
-        <p>
-          Buy negatron cloak!
-        </p>
+      <div className="relative bg-gradient-to-b from-yellow-300">
+
+      {/* Content */}
+      <div className="relative z-10 px-6 pt-14 lg:px-8">
+      <div id="signInButton">
+        <GoogleLogin
+          clientID={clientId}
+          buttonText="Login"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true}
+          />
       </div>
+
+    </div>
+
+  </div>
     )
   }
+}
   
   export default Login
