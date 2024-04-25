920f70a5d98572af95908b999e0f2eea885bdb85
rich2025 <richy@bu.edu>
2024-04-24 22:47:11 -0400

    final footer
    i dont wanna do this anymore

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,19 +1,22 @@
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-b from-yellow-50 to-yellow-200 text-black p-4 md:p-8">
-      <div className="max-w-screen-xl mx-auto flex justify-end space-x-12">
-        <h3 className="text-base font-bold">
+    <footer className="bg-gradient-to-b from-yellow-100 to-yellow-300 text-black p-4 md:p-8">
+      <div className="max-w-screen-xl mx-auto flex justify-end space-x-12 items-center"> {/* Added right padding */}
+        <h3 className="text-lg font-bold">
           <a href="/" className="hover:text-gray-300">Home</a>
         </h3>
-        <h3 className="text-base font-bold">
+        <h3 className="text-lg font-bold">
           <a href="/login" className="hover:text-gray-300">Login</a>
         </h3>
-        <h3 className="text-base font-bold">
+        <h3 className="text-lg font-bold">
           <a href="/about" className="hover:text-gray-300">About</a>
         </h3>
-        <h3 className="text-base font-bold">
-          <a href="https://github.com/rich2025/411project" className="hover:text-gray-300">GitHub</a>
-        </h3>
+        <div className="flex items-center space-x-2">
+          <h3 className="text-lg font-bold">
+            <a href="https://github.com/rich2025/411project" className="hover:text-gray-300">GitHub</a>
+          </h3>
+          <img src="https://t3.ftcdn.net/jpg/05/40/26/42/360_F_540264274_yeiC6K2XrI2nP8fmDJQl55qlAhZ1KPiC.webp" style={{ height: '2.5rem', position: 'relative', top: '-4.1rem' }} />
+        </div>
       </div>
     </footer>
   );
